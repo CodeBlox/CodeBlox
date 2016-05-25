aba603929147d6c421d7483ed892d98ca5b7fd90
liron_e <lironerman7@gmail.com>
2016-05-26 01:23:33 +0300

    no message

--- server/controllers/codeblox.js
+++ server/controllers/codeblox.js
@@ -52,60 +52,69 @@ module.exports.saveProject = function(req, res, next) {
 };
 
 module.exports.deleteFunc = function(req, res, next) {
-    getFileInDir(config.tmpDir + 'extract/' + req.params.name, function(files) {
-        
-        var selectedFuncs = req.body.funcs;
-        var zip = new AdmZip();
-
-        for(var i = 0; i < files.length; i++){
-            files[i] = files[i].replace(config.tmpDir + 'extract\\' + req.params.name + '\\', '')
-        }
-        
-        Project.findOne({name: req.params.name}, function(err, project) {
-            if (err) throw err;
+    if (!req.params.name) {
+        res.status(400).json({ error: 'No project name given'});
+    } else {
+        getFileInDir(config.tmpDir + 'extract/' + req.params.name, function(files) {
             
-            var functions = [];
-            for(var i = 0; i < project.functions.length; i++){
-                if (selectedFuncs.indexOf(project.functions[i].name) != -1) {
-                    functions.push({
-                        name: project.functions[i].name,
-                        file: project.functions[i].file
-                    });
-                }
+            var selectedFuncs = req.body.funcs;
+            
+            if (!selectedFuncs){
+                selectedFuncs = [];
             }
             
+            var zip = new AdmZip();
+
             for(var i = 0; i < files.length; i++){
+                files[i] = files[i].replace(config.tmpDir + 'extract\\' + req.params.name + '\\', '')
+            }
+            
+            Project.findOne({name: req.params.name}, function(err, project) {
+                if (err) throw err;
                 
-                var bIsFound = false;
-                
-                for (var j = 0; j < functions.length; j++) {
-                    if(functions[j].file === files[i]) {
-                        bIsFound = true;
-                        
-                        var fileContent = fs.readFileSync(config.tmpDir + 'extract\\' + req.params.name + '\\' + files[i], "utf8");
-                    
-                        var re = new RegExp("<!--.*{CodeBlox\\+" + functions[j].name + "}.*-->[^]*<!--.*{CodeBlox\\-" + functions[j].name + "}.*-->", "gmi");
-                        var match = re.exec(fileContent);
-                        
-                        if (match) {
-                            fileContent = fileContent.replace(match[0], "");
-                        }
-                        
-                        zip.addFile(functions[j].file, new Buffer(fileContent));
+                var functions = [];
+                for(var i = 0; i < project.functions.length; i++){
+                    if (selectedFuncs.indexOf(project.functions[i].name) != -1) {
+                        functions.push({
+                            name: project.functions[i].name,
+                            file: project.functions[i].file
+                        });
                     }
                 }
                 
-                if (!bIsFound) {
-                    zip.addLocalFile(config.tmpDir + 'extract\\' + req.params.name + '\\' + files[i],
-                                     files[i].replace(config.tmpDir + 'extract\\' + req.params.name + '\\', ''));
+                for(var i = 0; i < files.length; i++){
+                    
+                    var bIsFound = false;
+                    
+                    for (var j = 0; j < functions.length; j++) {
+                        if(functions[j].file === files[i]) {
+                            bIsFound = true;
+                            
+                            var fileContent = fs.readFileSync(config.tmpDir + 'extract\\' + req.params.name + '\\' + files[i], "utf8");
+                        
+                            var re = new RegExp("<!--.*{CodeBlox\\+" + functions[j].name + "}.*-->[^]*<!--.*{CodeBlox\\-" + functions[j].name + "}.*-->", "gmi");
+                            var match = re.exec(fileContent);
+                            
+                            if (match) {
+                                fileContent = fileContent.replace(match[0], "");
+                            }
+                            
+                            zip.addFile(functions[j].file, new Buffer(fileContent));
+                        }
+                    }
+                    
+                    if (!bIsFound) {
+                        zip.addLocalFile(config.tmpDir + 'extract\\' + req.params.name + '\\' + files[i],
+                                        files[i].replace(config.tmpDir + 'extract\\' + req.params.name + '\\', ''));
+                    }
                 }
-            }
-            var uid = randomstring.generate(5);
-            zip.writeZip(config.tmpDir + 'codeblox/' + req.params.name + '-' + uid + '.zip');
-            
-            res.download(config.tmpDir + 'codeblox/' + req.params.name + '-' + uid + '.zip');
+                var uid = randomstring.generate(5);
+                zip.writeZip(config.tmpDir + 'codeblox/' + req.params.name + '-' + uid + '.zip');
+                
+                res.download(config.tmpDir + 'codeblox/' + req.params.name + '-' + uid + '.zip');
+            });
         });
-    });
+    }
 };
 
 function getFileInDir(dir, _callback) {
